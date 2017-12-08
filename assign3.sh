3370fff4855a12d6251b8dcd0ca92a64c1d62257
Jackson Huang <huang.jizhou@wustl.edu>
2017-12-07 20:34:01 -0600

    update

--- assign3.sh
+++ assign3.sh
@@ -14,6 +14,7 @@ for nr in ${nr_val[*]}; do
 		for npf in ${npf_val[*]}; do
 			$GEM5/build/X86/gem5.opt hw3config.py -c daxpy/daxpy --cpu-type="DerivO3CPU" --caches --l2cache --npf_regs=$npf --nr_entries=$nr --ni_entries=$ni > /dev/null
 			cat m5out/stats.txt | grep -Ei "$patterns" | grep -Eo '\s+[0-9]+|\s+[0-9]\.[0-9]+' | sed 's/\s*//g'
+			rm core* > /dev/null
 		done
 	done
 done > "report.dat"
