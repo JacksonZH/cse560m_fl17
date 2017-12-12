6d4bbff2431c3e3096557e55f07abac40a8e0d1d
Jackson Huang <huang.jizhou@wustl.edu>
2017-12-11 21:47:44 -0600

    update

--- hw4opts.py
+++ hw4opts.py
@@ -11,13 +11,13 @@ def addHW4Opts(parser):
 	# available policies: SingleThread RoundRobin Branch IQCount LSQCount
 	parser.add_option('--fetch_policy', type="str", default="SingleThread")
 	# available policies: Dynamic Partitioned Threshold
-	parser.add_option('--LSQ_policy', type="str", default="Dynamic")
+	parser.add_option('--LSQ_policy', type="str", default="Partitioned")
 	# available policies: Dynamic Partitioned Threshold
-	parser.add_option('--IQ_policy', type="str", default="Dynamic")
+	parser.add_option('--IQ_policy', type="str", default="Partitioned")
 	# available policies: Dynamic Partitioned Threshold
-	parser.add_option('--ROB_policy', type="str", default="Dynamic")
+	parser.add_option('--ROB_policy', type="str", default="Partitioned")
 	# available policies: Aggressive RoundRobin OldestReady
-	parser.add_option('--commit_policy', type="str", default="Aggressive")
+	parser.add_option('--commit_policy', type="str", default="RoundRobin")
 
 #set parameters taken in from options on command line
 def set_config(cpu_list, options):
@@ -26,7 +26,7 @@ def set_config(cpu_list, options):
     # cpu.numROBEntries = options.nr_entries
     # cpu.numIQEntries = options.ni_entries
     cpu.numPhysFloatRegs = options.npf_regs
-    cpu.smtFetchPolicy = options.fetch_policy
+    # cpu.smtFetchPolicy = options.fetch_policy
     # cpu.smtLSQPolicy = options.LSQ_policy
     # cpu.smtIQPolicy = options.IQ_policy
     # cpu.smtROBPolicy = options.ROB_policy
