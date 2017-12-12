019019fbd6b46e3bce1c941a7079e82b648d40bf
Jackson Huang <huang.jizhou@wustl.edu>
2017-12-11 21:23:29 -0600

    update

--- hw4opts.py
+++ hw4opts.py
@@ -9,11 +9,9 @@ def addHW4Opts(parser):
 	parser.add_option('--ni_entries', type="int", default=64)
 	parser.add_option('--npf_regs', type="int", default=1024)
 	# available policies: SingleThread RoundRobin Branch IQCount LSQCount
-	# parser.add_option('--fetch_policy', type="str", default="SingleThread")
-	parser.add_option('--fetch_policy', type="str")
+	parser.add_option('--fetch_policy', type="str", default="SingleThread")
 	# available policies: Dynamic Partitioned Threshold
-	# parser.add_option('--ROB_policy', type="str", default="Dynamic")
-	parser.add_option('--ROB_policy', type="str")
+	parser.add_option('--ROB_policy', type="str", default="Partitioned")
 
 #set parameters taken in from options on command line
 def set_config(cpu_list, options):
@@ -22,7 +20,5 @@ def set_config(cpu_list, options):
     cpu.numROBEntries = options.nr_entries
     cpu.numIQEntries = options.ni_entries
     cpu.numPhysFloatRegs = options.npf_regs
-    if options.fetch_policy:
-	    cpu.smtFetchPolicy = options.fetch_policy
-    if options.ROB_policy:
-	    cpu.smtROBPolicy = options.ROB_policy
+    cpu.smtFetchPolicy = options.fetch_policy
+    cpu.smtROBPolicy = options.ROB_policy
